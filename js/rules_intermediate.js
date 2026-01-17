0474f3c6beed0b5193854512104068bfaf5046b8
二叉树树 <acofork@qq.com>
2026-01-17 18:57:20 +0800

    Add short link: cE9JsGA

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2634,5 +2634,9 @@ window.RULES_INTERMEDIATE = {
     "/t1N65AF": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/cE9JsGA": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
