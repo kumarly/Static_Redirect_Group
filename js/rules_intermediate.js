68f4cf9d1b8779cb7b8847cb42233382a65c9ca3
二叉树树 <acofork@qq.com>
2026-01-17 18:57:18 +0800

    Add short link: oMZdeDr

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2626,5 +2626,9 @@ window.RULES_INTERMEDIATE = {
     "/ZCXzhtF": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/oMZdeDr": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
