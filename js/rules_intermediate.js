3d610ad114ff838595bf69fa8856a68d93290dcb
二叉树树 <acofork@qq.com>
2026-01-17 18:48:26 +0800

    Add short link: YLFuz4N

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2202,5 +2202,9 @@ window.RULES_INTERMEDIATE = {
     "/ScVEG4X": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/YLFuz4N": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
