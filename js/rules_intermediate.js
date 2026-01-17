f0c00a38d96ca4cd51ce774bb0c679c55b768c76
二叉树树 <acofork@qq.com>
2026-01-17 18:49:11 +0800

    Add short link: QBv6kNI

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2266,5 +2266,9 @@ window.RULES_INTERMEDIATE = {
     "/RNPj6aU": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/QBv6kNI": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
