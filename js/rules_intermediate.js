d7a81461ea26c7d8fa08e466c85fbfa2c45ce40e
二叉树树 <acofork@qq.com>
2026-01-21 19:29:10 +0800

    Add short link: 3sihejb

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -4102,5 +4102,9 @@ window.RULES_INTERMEDIATE = {
     "/mtqfpmom": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T11:29:09.000Z"
+    },
+    "/3sihejb": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T11:29:11.000Z"
     }
 };
