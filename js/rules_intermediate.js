510d73e79c1111a3e049fe49b557a7b1c6a25ab4
二叉树树 <acofork@qq.com>
2026-01-21 19:02:44 +0800

    Add short link: nuxw5wnetr

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2358,5 +2358,9 @@ window.RULES_INTERMEDIATE = {
     "/4mcswyts7i": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-28T10:49:00.000Z"
+    },
+    "/nuxw5wnetr": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-28T10:49:00.000Z"
     }
 };
