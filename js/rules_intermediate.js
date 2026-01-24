a1dc15fbef4d5db87dada30ba4a569a90ff3e74a
二叉树树 <acofork@qq.com>
2026-01-24 13:12:50 +0800

    Add short link: 3afe07df5f

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -7638,5 +7638,9 @@ window.RULES_INTERMEDIATE = {
     "/5498dbc6be": {
         "url": "https://bilibili.com",
         "expired_at": "2026-01-27T04:46:00.000Z"
+    },
+    "/3afe07df5f": {
+        "url": "https://bilibili.com",
+        "expired_at": "2026-01-27T04:46:00.000Z"
     }
 };
