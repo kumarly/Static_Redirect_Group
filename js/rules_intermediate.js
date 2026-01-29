8339a88e981d36d0404ab0b4fbf19437a2127dc5
二叉树树 <acofork@qq.com>
2026-01-29 18:20:34 +0800

    Add short link: yqyhome

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2506,5 +2506,9 @@ window.RULES_INTERMEDIATE = {
     "/asdfghjkl": {
         "url": "https://iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.in/iIIIIiIIiiIiiiIiiIIIiIiIiIIIiiIiiIIiIIiiiiIiiiIiiiIIIiIiiiIiiiIiiIIiIiiiiIIIiIiiiIIIiIiiiIIIiiiiiIIIiiIIiiIIIiIiiiIiIIIIiiIiIIIIiiIIiiIiiIIIIiiiiiIiIIIiiIIiIIIiiIIIIiIiiiIiIIIIiIIiiiiIiIIIiiIIiIIi",
         "expired_at": "2026-02-04T07:49:00.000Z"
+    },
+    "/yqyhome": {
+        "url": "https://yqypuls.cn",
+        "expired_at": "2026-01-30T10:20:00.000Z"
     }
 };
